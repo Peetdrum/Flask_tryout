adc703cd2145f77edc3aedcc12ac112d8756630d
Peter <peetdrum@gmail.com>
2023-05-26 12:12:15 +0200

    basic flask, opdracht 6 en kleine correctie opmaak code

--- flask_app.py
+++ flask_app.py
@@ -3,7 +3,8 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route('/')
-def home():    return "<p>Hallo Peter!</p>"
+def home():    
+    return "<p>Hallo Peter!</p>"
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
