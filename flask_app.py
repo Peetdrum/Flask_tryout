b4bf2641368cc3bbff8ebdf43650fc43f0efa196
Peter <peetdrum@gmail.com>
2023-05-26 12:04:54 +0200

    basic Flask code + opdracht 6

--- flask_app.py
+++ flask_app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route('/')
-def home():    return "Hello world!"
+def home():    return "<p>Hallo Peter!</p>"
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
